a31d458bf41a9a5d4f92bfa527c2c067d82b1d43
amanda-oliveira21 <amanda.olv.72@gmail.com>
2025-05-05 15:17:57 -0300

    Update test_cadastro_cliente.py

--- teste_amanda/test_cadastro_cliente.py
+++ teste_amanda/test_cadastro_cliente.py
@@ -1,5 +1,5 @@
 from selenium import webdriver
-from selenium.webdriver.commom.by import By 
+from selenium.webdriver.common.by import By 
 import time
 
 #Configuração do WebDriver (nesse exemplo, estamos usando o Chrome)
@@ -9,26 +9,30 @@ driver = webdriver.Chrome()
 # com o protocolo file://
 # Certifique-se de que o caminho está apontando para um arquivo
 # HTML específico
-driver.get("file:///C:\Users\amanda_oliveira22\Documents\GitHub\AmandaOliveira_Desen_Sistemas_Tarde\teste_amanda\index.html")
+driver.get("file:///C:/Users/amanda_oliveira22/Documents/GitHub/AmandaOliveira_Desen_SistemasTarde/teste_amanda/index.html")
+
+# Preencha o campo nome
+nome_input = driver.find_element(By.ID, "name")
+nome_input.send_keys("João Silva")
 
 # Preencha o campo CPF
 cpf_input = driver.find_element(By.ID, "cpf")
 cpf_input.send_keys("12345678901")
 
 # Preencha o campo Endereço
-cpf_input = driver.find_element(By.ID, "address")
-cpf_input.send_keys("Rua das Flores")
+endereco_input = driver.find_element(By.ID, "address")
+endereco_input.send_keys("Rua das Flores")
 
 # Preencha o campo Telefone
-cpf_input = driver.find_element(By.ID, "phone")
-cpf_input.send_keys("119887654321")
+telefone_input = driver.find_element(By.ID, "phone")
+telefone_input.send_keys("119887654321")
 
 #Clica no botão de Cadastrar
 submit_buttom= driver.find_element(By.CSS_SELECTOR, "button[type='submit']")
 submit_buttom.click()
 
 #Aguarde um momento para visualizar o resultado (em uma aplicação real, você verificaria a resposta)
-time.sleep(3)
+time.sleep(600)
 
 #Fecha o navegador
 driver.quit()
